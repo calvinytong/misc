7453c0180b3ce4a9d2dc6abf4dcd3c16dc39eb34
calvinytong <calvinytong@gmail.com>
2017-02-15 19:54:43 -0500

    add fix_authorship.sh

---
+++ fix_authorship.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# This script was taken from the github tutorial for changing authorship. It can
+# Be found here https://help.github.com/articles/changing-author-info/
+# Replace OLD_EMAIL, CORRECT_NAME, CORRECT_EMAIL with desired fields and run in
+# a bare cloned repo
+git filter-branch --env-filter '
+
+OLD_EMAIL=""
+CORRECT_NAME=""
+CORRECT_EMAIL=""
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
